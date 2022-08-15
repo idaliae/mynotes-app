a2f78fb77c294bdec89e9cedb30df6b0045666cd
Idalia Z. Espino Lira <idalia.espino12@gmail.com>
2022-08-14 23:27:14 -0500

    Notes

--- src/components/Note.js
+++ src/components/Note.js
@@ -3,22 +3,53 @@ import '../css/Note.css';
 import PropTypes from 'prop-types';
 
 class Note extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
+    this.titleContent = React.createRef();
+    this.bodyContent = React.createRef();
+    this.state = {
+        title: this.props.title,
+        body: this.props.body,
+        editMode: false
+    }
   }
 
-  render() {
+  handleEdit=()=>{
+    this.setState({
+        editMode: true
+    });
+  }
+
+  handleSave=()=>{
+    this.setState({
+        title: this.titleContent.current.value,
+        body: this.bodyContent.current.value,
+        editMode: false
+    });
+  }
+
+  render(){
+    let titleElement, bodyElement, buttonArea; 
+    if (this.state.editMode){
+      titleElement = <textarea ref={this.titleContent} className="title-textarea" defaultValue={this.state.title}></textarea>;
+      bodyElement = <textarea ref={this.bodyContent} className="body-textarea" defaultValue={this.state.body}></textarea>;
+      buttonArea = <div><button className="btn btn-primary" onClick={this.handleSave}>Save</button></div>;
+    }
+    else{
+      titleElement = <h5 className="card-title">{this.state.title}</h5>;
+      bodyElement = <p>{this.state.body}</p>; 
+      buttonArea = <div><button className="btn btn-info" onClick={this.handleEdit}>Edit</button><button className="btn btn-danger" onClick={this.handleDelete}>Delete</button></div>;
+    }
     return (
-      <div className="col-sm-6">
-        <div className="card card-view">
-          <div className="card-body">
-            <h5 className="card-title">{this.props.title}</h5>
-            <p>{this.props.body}</p>
-            <button className="btn btn-info">Edit</button>
-            <button className="btn btn-danger">Delete</button>
-          </div>
+          <div className='col-sm-6'>
+          <div className="card card-view">
+              <div className="card-body">
+                {titleElement}
+                {bodyElement}
+                {buttonArea}
+                </div>
+            </div>
         </div>
-      </div>
     );
   }
 }
